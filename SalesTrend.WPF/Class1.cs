b981b3e3366c67e7a28f289ab505c656af9d877d
m.shkolin <m.shkolin@w1.money>
2023-12-10 17:47:17 +0500

    Добавьте файлы проекта.

---
+++ SalesTrend.WPF/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTrend.WPF
+{
+    internal class Class1
+    {
+    }
+}
